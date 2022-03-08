c4870b6cb7a1b585aa6fe02b5833b5522b170703
unknown <kuredema7@gmial.com>
2022-03-08 20:52:25 +0300

    refined the if statement

--- assignment/LeapYear.java
+++ assignment/LeapYear.java
@@ -19,12 +19,12 @@ public class LeapYear {
         if (year % 4 == 0) {
             if (year % 400 == 0) {
                 System.out.println(year + " is a leap year.");
+            }
+            if (year % 100 == 0) {
+                System.out.println(year + " is not a leap year");
             } else {
-                if (year % 100 == 0) {
-                    System.out.println(year + " is not a leap year");
-                } else {
-                    System.out.println(year + " is a leap year");
-                }
+                System.out.println(year + " is a leap year");
+
             }
         }
 
